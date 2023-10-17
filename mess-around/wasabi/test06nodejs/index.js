b5b8d493ce8b2f01e51fe5f7d4c3e3b8d697df3c
Jakob Getz <jakob@getz.de>
2023-10-17 15:32:22 +0900

    test06nodejs

---
+++ mess-around/wasabi/test06nodejs/index.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import path from 'path'
+const p = path.join(import.meta.dir, 'index.wasm')
+const wasmBinary = fs.readFileSync(p)
+
+const Wasabi = require('./index.wasabi.js')
+
+Wasabi.analysis = {
+    call_pre(location, targetFunc, args, indirectTableIdx) {
+        console.log(`TAAARGETT: ${targetFunc}`)
+        console.log(location, (indirectTableIdx === undefined ? "direct" : "indirect"), "call", "to func #", targetFunc, "args =", args);
+    },
+};
+
+(async function () {
+    let instance
+    let imports = {}
+    let wasm = await WebAssembly.instantiate(wasmBinary, imports)
+    instance = wasm.instance
+    instance.exports.main()
+})()
