0a46da2a330bae8bdb4c68297b86c38e97743e3a
Jakob Getz <jakob@getz.de>
2023-11-20 22:38:05 +0900

    heatmap test to 10000ms

--- tests/online/heatmap/test.js
+++ tests/online/heatmap/test.js
@@ -3,6 +3,6 @@ import { delay } from '../../../dist/tests/test-utils.cjs'
 export default async function test(analyser) {
   const url = 'https://aurium.gitlab.io/wasm-heatmap/'
   const page = await analyser.start(url, { headless: true })
-  await delay(5000)
+  await delay(10000)
   return await analyser.stop()
 }
