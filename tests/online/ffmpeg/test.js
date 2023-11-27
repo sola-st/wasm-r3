53c8ed72219c4c6ab566a6f9f54535bd762bd196
Jakob Getz <jakob@getz.de>
2023-11-27 19:22:02 +0900

    ffmpeg and ogv test

--- tests/online/ffmpeg/test.js
+++ tests/online/ffmpeg/test.js
@@ -2,11 +2,17 @@ import { delay } from '../../../dist/tests/test-utils.cjs'
 
 export default async function test(analyser) {
   const url = 'https://w3reality.github.io/async-thread-worker/examples/wasm-ffmpeg/index.html'
-  const page = await analyser.start(url, { headless: true })
+  const page = await analyser.start(url, { headless: false })
+  // const estimateButton = page.locator('#devHelpButton')
+  // const logWindow = page.locator('#log')
+  // await estimateButton.waitFor({ state: 'visible' })
+  // await logWindow.waitFor({ state: 'visible' })
+  // await estimateButton.click()
+  // await logWindow.waitFor(() => this.textContent().includes('set the subtitle options to the indicated preset'), { timeout: 240000 })
+  await delay(3000)
+  const button = await page.$('#devHelpButton')
+  await delay(1000)
+  button.click()
   await delay(5000)
-  // TODO
-  // - upload mp4
-  // - click estimate
-  // - click convert takes potentially asctronomically long
   return await analyser.stop()
 }
