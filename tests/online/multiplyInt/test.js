a7ffe8b580e58ce65c4f34ad798d66b23c05898c
Yusung Sim <yusungsim@kaist.ac.kr>
2023-11-29 17:07:30 +0900

    Added online tests : takahirox - multiplyInt, multiplyDouble

---
+++ tests/online/multiplyInt/test.js
@@ -0,0 +1,16 @@
+import { delay } from '../../../dist/tests/test-utils.cjs'
+
+export default async function test(analyser) {
+  const url = 'https://takahirox.github.io/WebAssembly-benchmark/tests/multiplyInt.html' 
+  let page = await analyser.start(url, { headless: true })
+
+  const runLoc = page.locator('#run_button')
+  await runLoc.waitFor({state: 'visible'}) 
+  const doneLoc = page.locator('#message')
+
+  await runLoc.click()
+  await doneLoc.waitFor({state: 'visible', timeout: 240000})
+  await doneLoc.waitFor(() => this.textContent().includes('Done'), {timeout: 240000})
+
+  return await analyser.stop()
+}
