474c5eca14afce9dd462319bcb9c2f8f649c2df3
Jakob Getz <jakob@getz.de>
2023-12-01 20:54:16 +0900

    adjust test and perf measure

--- tests/online/ogv/test.js
+++ tests/online/ogv/test.js
@@ -3,6 +3,6 @@ import { delay } from '../../../dist/tests/test-utils.cjs'
 export default async function test(analyser) {
   const url = 'https://brionv.com/misc/ogv.js/demo/'
   const page = await analyser.start(url, { headless: true })
-  await delay(5000)
+  await delay(7000)
   return await analyser.stop()
 }
