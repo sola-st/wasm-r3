f598812cca042dfb83570729a42f7d81f50c6d6c
Yusung Sim <yusungsim@kaist.ac.kr>
2024-02-06 17:13:57 +0900

    modified gol script

--- tests/online/game-of-life/test.js
+++ tests/online/game-of-life/test.js
@@ -1,12 +1,22 @@
 import { delay } from '../../../dist/tests/test-utils.cjs'
 
 export default async function test(analyser) {
-    const url = 'https://playgameoflife.com/'
-    const page = await analyser.start(url, { headless: true })
-    const button = page.locator('#start')
-    await button.waitFor({ state: 'visible' })
-    await button.click()
-    await delay(500)
-    await button.click()
-    return await analyser.stop()
+  const url = 'https://playgameoflife.com/'
+  const page = await analyser.start(url, { headless: false })
+
+  // const button = page.locator('#start')
+  // await button.waitFor({ state: 'visible' })
+  // await button.click()
+  // await delay(500)
+  // await button.click()
+
+  const nextButton = page.locator('#button')
+  await nextButton.wasabi({state: 'visible'})
+  const N = 5
+  for (let i = 0; i < N; i++) {
+    await nextButton.click()
+    await delay(1000)
+  }
+
+  return await analyser.stop()
 }
