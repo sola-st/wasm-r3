16aaa79976baa1c077213b3348910385e011658d
Yusung Sim <yusungsim@kaist.ac.kr>
2024-02-07 17:03:08 +0900

    Modified factorial test case

--- tests/online/factorial/test.js
+++ tests/online/factorial/test.js
@@ -12,16 +12,19 @@ export default async function test(analyser) {
   await numberOut.waitFor({state: 'visible'})
 
   await textInput.fill('')  
+  await textInput.press('1')
+  await textInput.press('0')
+  await expect(numberOut).toContainText('fact(10)', {timeout: 10000})
 
-  await textInput.fill('0')  
-  await expect(numberOut).toContainText('fact(0)', {timeout: 10000})
+  await textInput.fill('')  
+  await textInput.press('2')
+  await textInput.press('0')
+  await expect(numberOut).toContainText('fact(20)', {timeout: 10000})
 
-  await textInput.fill('1')  
-  await expect(numberOut).toContainText('fact(1)', {timeout: 10000})
-
-  await textInput.fill('2')  
-  await expect(numberOut).toContainText('fact(2)', {timeout: 10000})
-
+  await textInput.fill('')  
+  await textInput.press('4')
+  await textInput.press('2')
+  await expect(numberOut).toContainText('fact(42)', {timeout: 10000})
 
   return await analyser.stop()
 }
