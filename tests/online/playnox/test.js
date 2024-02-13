2714a336fbe58bea7a8f55610132e9ed960397b7
Yusung Sim <yusungsim@kaist.ac.kr>
2024-02-13 21:26:28 +0900

    Added playnox

---
+++ tests/online/playnox/test.js
@@ -0,0 +1,25 @@
+import { delay } from '../../../dist/tests/test-utils.cjs'
+
+export default async function test(analyser) {
+  const url = 'https://playnox.xyz/'
+  const page = await analyser.start(url, { headless: false })
+
+  const goButton = page.getByText('Yes, I like data.')
+  await goButton.waitFor({state: 'visible'})
+  await goButton.click()
+
+  const canv = page.locator('#canvas')
+  await canv.waitFor({state: 'visible'})
+
+  
+  await delay(10000)
+  await canv.click({
+    button: 'left',
+    position: {x: 725, y: 446},
+  })
+  console.log('clicked')
+
+  // await delay(10000)
+
+  return await analyser.stop()
+}
