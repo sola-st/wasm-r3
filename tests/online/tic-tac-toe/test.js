d7271270ae5326b2385171282b9d26d99f978763
Yusung Sim <yusungsim@kaist.ac.kr>
2024-02-14 16:24:47 +0900

    Added test tic-tac-toe

---
+++ tests/online/tic-tac-toe/test.js
@@ -0,0 +1,21 @@
+import { delay } from '../../../dist/tests/test-utils.cjs'
+
+export default async function test(analyser) {
+  const url = 'https://sepiropht.github.io/tic-tac-toe-wasm/'
+  const page = await analyser.start(url, { headless: true })
+ 
+  const sizeSelect = page.getByLabel('Size of the board')
+  await sizeSelect.waitFor({state: 'visible'})
+
+  await sizeSelect.fill('3')
+
+  /*
+  const upperright = page.locator('[data-cell="0_2"]')
+  await upperright.waitFor({state: 'visible'})
+  await upperright.click()
+  */
+
+  await delay(1000)
+
+  return await analyser.stop()
+}
