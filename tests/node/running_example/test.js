eb814d45b7e868d1a745863bfc564c8689baf5f5
Doehyun Baek <doehyunbaek@gmail.com>
2024-03-12 20:41:53 +0900

    add running_example

---
+++ tests/node/running_example/test.js
@@ -0,0 +1,7 @@
+export default async function test(wasmBinary) {
+    let instance; let imports = { env: {} }
+    imports.env.changeMem = (k, v) => { new Uint8Array(instance.exports.memory.buffer)[k] = v }
+    let wasm = await WebAssembly.instantiate(wasmBinary, imports)
+    instance = wasm.instance
+    instance.exports.entry()
+}
