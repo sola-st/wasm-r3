288edce92fe7d4ab65c6b1ea7a1db753001ed4f2
Yusung Sim <yusungsim@kaist.ac.kr>
2024-03-26 13:44:59 +0900

    parquet

---
+++ tests/online/parquet/test.js
@@ -0,0 +1,11 @@
+import { delay } from '../../../dist/tests/test-utils.cjs'
+
+export default async function test(analyser) {
+  const url = 'https://google.github.io/filament/webgl/parquet.html'
+  const page = await analyser.start(url, { headless: false })
+
+  // the webpage is just showcase.. so just wait
+  await delay(10000)
+
+  return await analyser.stop()
+}
