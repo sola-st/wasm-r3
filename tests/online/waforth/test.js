5293acbf321bf60b2d6b994d658a41f4c2eedf1b
Jakob Getz <jakob@getz.de>
2024-03-27 12:23:54 +0100

    fix waforth test script

--- tests/online/waforth/test.js
+++ tests/online/waforth/test.js
@@ -4,15 +4,15 @@ export default async function test(analyser) {
   const url = 'https://el-tramo.be/waforth'
   const page = await analyser.start(url, { headless: true })
   
-  const textInput = page.getByRole('pre')
+  const textInput = page.locator('.Console')
   await textInput.waitFor({state: 'visible'})
   console.log(1)
 
-  await delay(5000)
+  await delay(2000)
 
   await textInput.pressSequentially('asdf\n')
 
-  await delay(10000)
+  await delay(2000)
 
   return await analyser.stop()
 }
