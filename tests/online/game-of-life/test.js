e61b03a1b4c00c32f904bf0220d9d531f74f8605
Doehyun Baek <doehyunbaek@gmail.com>
2024-04-01 20:13:32 +0900

    revert game-of-life test script change
    This commit made the test more flaky, reverting
    for now
    https://github.com/jakobgetz/wasm-r3/commit/f598812cca042dfb83570729a42f7d81f50c6d6c

--- tests/online/game-of-life/test.js
+++ tests/online/game-of-life/test.js
@@ -1,22 +1,12 @@
 import { delay } from '../../../dist/tests/test-utils.cjs'
 
 export default async function test(analyser) {
-  const url = 'https://playgameoflife.com/'
-  const page = await analyser.start(url, { headless: true })
-
-  // const button = page.locator('#start')
-  // await button.waitFor({ state: 'visible' })
-  // await button.click()
-  // await delay(500)
-  // await button.click()
-
-  const nextButton = page.locator('#next')
-  await nextButton.waitFor({ state: 'visible' })
-  const N = 10
-  for (let i = 0; i < N; i++) {
-    await nextButton.click()
-    await delay(1000)
-  }
-
-  return await analyser.stop()
+    const url = 'https://playgameoflife.com/'
+    const page = await analyser.start(url, { headless: true })
+    const button = page.locator('#start')
+    await button.waitFor({ state: 'visible' })
+    await button.click()
+    await delay(500)
+    await button.click()
+    return await analyser.stop()
 }
