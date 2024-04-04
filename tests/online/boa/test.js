2884d492dc6916c4a51fa4127833ab416f09572c
Doehyun Baek <doehyunbaek@gmail.com>
2024-04-04 15:56:27 +0900

    fix boa failing
    Probably due to some website changes, the test was failing.
    This commit fixes this.

--- tests/online/boa/test.js
+++ tests/online/boa/test.js
@@ -4,13 +4,8 @@ export default async function test(analyser) {
     const url = 'https://boajs.dev/boa/playground/'
     const page = await analyser.start(url, { headless: true })
     await delay(3000) // wait until instrumentation is done
-    // const terminalWrapper = page.locator('.textbox')
-    // await terminalWrapper.waitFor({ state: 'visible' })
-    // await delay(5000)
-    // await terminalWrapper.focus()
-    // doesnt work yet :(
-    // await terminalWrapper.type('console.log("yo mama!")');
-    // await delay(2000)
-    // page.pause()
+    await page.locator('div').filter({ hasText: /^greet\('World'\)$/ }).click();
+    await page.keyboard.press('Backspace');
+    await page.keyboard.press('Backspace');
     return await analyser.stop()
 }
